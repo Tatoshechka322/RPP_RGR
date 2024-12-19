7495480c3b4785d4d4b7c6c560d969ff722235b2
Clara <ershovaclara@yandex.ru>
2024-12-20 01:35:43 +0700

    index

--- app.py
+++ app.py
@@ -1,37 +1,75 @@
 import os
-import secrets
-from sqlite3 import IntegrityError
-from flask import Flask, request, redirect, jsonify, render_template, flash, url_for, abort
-from flask_caching import Cache
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
+import random
+import string
+from flask import Flask, request, redirect, render_template, flash, url_for
 from flask_login import login_user, LoginManager, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
-from models import db, ShortenedLink, users
-from config import Config
-
-app = Flask(__name__)
+from db import db
+from db.models import users, ShortenedLink
 
 # Инициализация компонентов
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://ershtrub:postgres@127.0.0.1:5432/BokovLarionov'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
 
 # Инициализация LoginManager
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
 
 # Загрузка пользователя по ID
 @login_manager.user_loader
 def load_users(user_id):
     return users.query.get(int(user_id))
 
-@app.route('/', methods=['GET'])
-def main():
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.query.get(int(user_id))
+
+def generate_short_id(length=6):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        original_url = request.form['original_url']
+        custom_short_id = request.form.get('custom_short_id')
+
+        if not original_url:
+            flash('Пожалуйста, введите URL', 'error')
+            return redirect(url_for('index'))
+
+        short_id = custom_short_id or generate_short_id()
+        existing_link = ShortenedLink.query.filter_by(short_id=short_id).first()
+        if existing_link:
+            flash('Этот короткий URL уже используется.', 'error')
+            return redirect(url_for('index'))
+
+        new_link = ShortenedLink(user_id=current_user.id if current_user.is_authenticated else None, original_url=original_url, short_id=short_id)
+        db.session.add(new_link)
+        db.session.commit()
+        flash(f'Ваш короткий URL: {url_for("redirect_to_original", short_id=short_id, _external=True)}', 'success')
+        return redirect(url_for('index'))
     return render_template('index.html')
+
+
+@app.route('/<short_id>')
+def redirect_to_original(short_id):
+    link = ShortenedLink.query.filter_by(short_id=short_id).first_or_404()
+    link.click_count += 1
+    db.session.commit()
+    return redirect(link.original_url)
+
 
 ## Страница Входа
 @app.route('/signin', methods=['GET', 'POST'])
-def login():
+def signin():
     if request.method == 'GET':
         return render_template('signin.html')
 
@@ -50,13 +88,13 @@ def login():
     else:
         login_user(user)
         flash('Вы успешно вошли!', 'success')
-        return redirect(url_for('index'))
+        return redirect(url_for('main'))
 
-    return render_template('login.html', errors=errors, login=login, password='')
+    return render_template('signin.html', errors=errors, login=login, password='')
 
 ## Страница Регистрации
 @app.route('/signup', methods=['GET', 'POST'])
-def register():
+def signup():
     if request.method == 'GET':
         return render_template('signup.html')
 
@@ -85,7 +123,7 @@ def register():
             return redirect(url_for('signin'))
         except Exception as e:
             db.session.rollback()
-    return render_template('signup.html', errors=errors, login=login, password='', password_check='')
+    return render_template('signin.html', errors=errors, login=login, password='', password_check='')
 
 
 
@@ -98,8 +136,5 @@ def logout():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Create tables if they don't exist
+        db.create_all()
     app.run(debug=True)
-
-
-
---
+++ db/models.py
@@ -0,0 +1,20 @@
+from . import db
+from flask_login import UserMixin
+
+class users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(1024))
+
+    def __repr__(self):
+        return f'id:{self.id}, login:{self.login}'
+
+class ShortenedLink(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  original_url = db.Column(db.String(255), nullable=False)
+  short_id = db.Column(db.String(6), unique=True, nullable=False)
+  click_count = db.Column(db.Integer, default=0)
+
+def __repr__(self):
+    return f'<ShortenedLink {self.short_id}>'
